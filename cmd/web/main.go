db2dd5d5a20422a7eab9752d89dcc371dcfc75ef
Thorsten Heimes <thorsten.heimes@bergzeit.de>
2023-01-13 09:07:58 +0100

    Added PaaS deployment

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/theimes/hello-api/handlers"
 	"github.com/theimes/hello-api/handlers/rest"
 )
 
 func main() {
-	addr := ":4000"
-
+	addr := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	if addr == ":" {
+		addr = "4000"
+	}
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/hello", rest.TranslateHandler)
+	mux.HandleFunc("/health", handlers.HealthCheck)
 
 	log.Printf("listening on %s", addr)
 
