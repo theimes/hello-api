74d1b65b3385ff9337705bf2e9dc36c5f06f27ed
Thorsten Heimes <thorsten.heimes@bergzeit.de>
2023-01-13 10:31:41 +0100

    Updated URL for translate endpoint

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -17,7 +17,7 @@ func main() {
 	}
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/hello", rest.TranslateHandler)
+	mux.HandleFunc("/translate/hello", rest.TranslateHandler)
 	mux.HandleFunc("/health", handlers.HealthCheck)
 
 	log.Printf("listening on %s", addr)
